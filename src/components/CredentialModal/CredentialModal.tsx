9f5ad3b75e96a430baee5535b92cd431da3b3c50
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-08-08 12:42:53 +0700

    Create--CredentialModal.tsx

---
+++ src/components/CredentialModal/CredentialModal.tsx
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import { Modal } from "react-bootstrap";
+import JSONPretty from "react-json-pretty";
+import "./CredentialModal.css";
+import { VID } from "../CredentialTypes/VID/VID";
+import { Diploma } from "../CredentialTypes/Diploma/Diploma";
+import { VP } from "../CredentialTypes/VP/VP";
+import * as transform from "../../utils/StringTransformation";
+import { IAttribute } from "../../dtos/attributes";
+
+type CallbackFunction = () => void;
+
+type Props = {
+  credential: IAttribute;
+  isModalCredentialOpen: boolean;
+  methodToClose: CallbackFunction;
+};
+
+type State = {
+  isFullCredentialDisplayed: boolean;
+  name: string;
+};
+
+class CredentialModal extends Component<Props, State> {
+  constructor(props: Readonly<Props>) {
+    super(props);
+
+    this.state = {
+      isFullCredentialDisplayed: false,
+      name: "",
+    };
+  }
+
+  openDetails = async (): Promise<void> => {
+    const { credential } = this.props;
+    const name = await transform.modifyName(
+      credential.name,
+      "credential",
+      credential.did
+    );
+    this.setState({
+      isFullCredentialDisplayed: true,
+      name,
+    });
+  };
+
+  closeDetails = (): void => {
+    const { isFullCredentialDisplayed } = this.state;
+    if (isFullCredentialDisplayed) {
+      this.setState({
+        isFullCredentialDisplayed: false,
+        name: "",
+      });
+    }
+  };
+
+  render(): JSX.Element {
+    const { methodToClose, credential, isModalCredentialOpen } = this.props;
+    const { isFullCredentialDisplayed, name } = this.state;
+    return (
+      <Modal show={isModalCredentialOpen} onHide={methodToClose} size="lg">
+        <Modal.Header className="ModalHeader" closeButton>
+          <Modal.Title className="ModalTitleCredential">{name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {!isFullCredentialDisplayed && isModalCredentialOpen && (
+            <>
+              {credential.name === "Verifiable ID" && (
+                <VID data={credential.dataDecoded || ""} />
+              )}
+              {credential.name === "VerifiablePresentation" && <VP />}
+              {(credential.name === '["Europass Diploma"]' ||
+                credential.name === "Europass Diploma") && (
+                <Diploma data={credential.dataDecoded || ""} />
+              )}
+            </>
+          )}
+          {isFullCredentialDisplayed && (
+            <JSONPretty id="json-pretty-store" data={credential.dataDecoded} />
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <button
+            className="ecl-button ecl-button--ghost"
+            type="button"
+            onClick={methodToClose}
+          >
+            Close
+          </button>
+          {!isFullCredentialDisplayed ? (
+            <button
+              className="ecl-button ecl-button--primary"
+              type="button"
+              onClick={this.openDetails}
+            >
+              Open Details
+            </button>
+          ) : (
+            <button
+              className="ecl-button ecl-button--primary"
+              type="button"
+              onClick={this.closeDetails}
+            >
+              Close Details
+            </button>
+          )}
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default CredentialModal;
