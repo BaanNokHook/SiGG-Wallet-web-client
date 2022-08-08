f1c037692fcacc69e78fd2bf8791dd0030cc72ed
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-08-08 14:00:34 +0700

    Create--Ribbon

---
+++ src/components/Ribbon/Ribbon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Ribbon.css";
+import REQUIRED_VARIABLES from "../../config/env";
+
+const DEMONSTRATOR_URL = REQUIRED_VARIABLES.REACT_APP_DEMO;
+
+export const Ribbon: React.FunctionComponent = () => (
+  <>
+    <Container>
+      <Row>
+        <Col className="col" />
+      </Row>
+    </Container>
+    <div className="ribbon" data-tut="reactour_header">
+      <a className="ribbonText" href={DEMONSTRATOR_URL}>
+        NEXT DEMO
+      </a>
+    </div>
+  </>
+);
+export default Ribbon;
